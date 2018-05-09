67769ba407ddb01f47b8ace0241d1d3fbcffe147
unknown <251892236@qq.com>
2018-05-09 09:50:47 +0800

    ll

---
+++ js/dataSelect.js
@@ -0,0 +1,284 @@
+var sYear={};
+var oDate = new Date();
+var year =  oDate.getFullYear();  // 年
+var month =  oDate.getMonth();  // 月( 0-11 )
+var date =  oDate.getDate();  // 日
+var dataBegin = oDate.getFullYear()
+
+sYear={
+
+};
+var niankk;
+
+var mySwiperNian,mySwiperYue,mySwiperTian;
+// 渲染
+function selectTime(selectNian) {
+    $('#data-mask').show();
+    $('.data-select').slideDown()
+    niankk = selectNian
+    nian()
+    yue();
+    tian();
+    mySwiperNian = new Swiper('.swiper-container-nian',{
+        direction: 'vertical',
+        height:'50'
+    });
+    mySwiperYue = new Swiper('.swiper-container-yue',{
+        direction: 'vertical',
+        height:'50'
+    });
+    mySwiperTian = new Swiper('.swiper-container-tian',{
+        direction: 'vertical',
+        height:'50',
+    })
+}
+// 渲染年
+function nian() {
+    let dataLi =$('<li class="swiper-slide" onclick="selectYears(this)"></li>')
+    $('.data-years').html('')
+    if(niankk < year){
+        for(let s=year;s>=niankk;s--){
+            dataLi.text(s)
+            $('.data-years').append(dataLi.clone())
+        }
+    }
+    else{
+        for(let i=niankk;i>=year;i--){
+            dataLi.text(i)
+            $('.data-years').append(dataLi.clone())
+        }
+    }
+
+}
+
+// 选择年
+function selectYears(event) {
+    sYear = {
+        n:event.innerHTML,
+        y:null,
+        d:null
+    };
+    $(event).addClass('current').siblings().removeClass('current');
+    oDate.setFullYear(sYear.n);
+    yue()
+}
+
+
+
+// 渲染月
+function yue() {
+    let dataLi2 =$('<li class="swiper-slide" onclick="selectMonth(this)"></li>')
+    let dataLino =$('<li class="swiper-slide col999"></li>')
+    let arr=["01","02","03","04","05","06","07","08","09","10","11","12"];
+    $('.data-month').html('')
+    if(niankk< dataBegin){
+        if(sYear.n == niankk){
+            for(let y=0;y<arr.length;y++){
+                if(y<month){
+                    dataLino.text(arr[y]);
+                    $('.data-month').append(dataLino.clone())
+                }else{
+                    dataLi2.text(arr[y]);
+                    $('.data-month').append(dataLi2.clone())
+                }
+
+            }
+        }
+        else if(sYear.n == dataBegin){
+            for(let y=0;y<arr.length;y++){
+                if(y>month){
+                    dataLino.text(arr[y]);
+                    $('.data-month').append(dataLino.clone())
+                }else{
+                    dataLi2.text(arr[y]);
+                    $('.data-month').append(dataLi2.clone())
+                }
+
+            }
+        }
+        else{
+            for(let y=0;y<arr.length;y++){
+                dataLi2.text(arr[y]);
+                $('.data-month').append(dataLi2.clone())
+            }
+
+
+        }
+    }else{
+        if(sYear.n == dataBegin){
+            for(let y=0;y<arr.length;y++){
+                if(y<month){
+                    dataLino.text(arr[y]);
+                    $('.data-month').append(dataLino.clone())
+                }else{
+                    dataLi2.text(arr[y]);
+                    $('.data-month').append(dataLi2.clone())
+                }
+
+            }
+
+        }
+        else if(sYear.n == niankk){
+            for(let y=0;y<arr.length;y++){
+                if(y>month){
+                    dataLino.text(arr[y]);
+                    $('.data-month').append(dataLino.clone())
+                }else{
+                    dataLi2.text(arr[y]);
+                    $('.data-month').append(dataLi2.clone())
+                }
+
+            }
+        }
+        else{
+            for(let y=0;y<arr.length;y++){
+                dataLi2.text(arr[y]);
+                $('.data-month').append(dataLi2.clone())
+            }
+        }
+    }
+}
+
+
+// 渲染天
+function tian(p) {
+    let nian = oDate.getFullYear()
+    let dataLi3 =$('<li class="swiper-slide" onclick="selectDay(this)"></li>')
+    let dataLino =$('<li class="swiper-slide col999"></li>')
+    let yue = oDate.getMonth(); //当前月
+    let setDat = new Date(nian,yue,1 - 1);
+    let setTian = oDate.getDate();
+    // 判断是否是闰年(29) 平年(28)
+    function isLeapYear() {
+        if(((nian % 4)==0) && ((nian % 100)!=0) || ((nian % 400)==0)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    let monthDaysArr = [31, 28+isLeapYear(), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    $('.data-date').html('')
+
+    if(niankk< dataBegin){
+        if(sYear.n == dataBegin && sYear.y == month+1){
+            for(let s=1;s<= monthDaysArr[yue]; s++){
+
+                if(s > p){
+                    dataLino.text(s);
+                    $('.data-date').append(dataLino.clone())
+                }else{
+                    dataLi3.text(s);
+                    $('.data-date').append(dataLi3.clone())
+                }
+
+            }
+        }
+        else{
+            for(let s=1;s<= monthDaysArr[yue]; s++){
+
+                if(s < p){
+                    dataLino.text(s);
+                    $('.data-date').append(dataLino.clone())
+                }else{
+                    dataLi3.text(s);
+                    $('.data-date').append(dataLi3.clone())
+                }
+
+            }
+        }
+    }else{
+        if(sYear.n == niankk && sYear.y == month+1){
+            for(let s=1;s<= monthDaysArr[yue]; s++){
+
+                if(s > p){
+                    dataLino.text(s);
+                    $('.data-date').append(dataLino.clone())
+                }else{
+                    dataLi3.text(s);
+                    $('.data-date').append(dataLi3.clone())
+                }
+
+            }
+        }
+        else{
+            for(let s=1;s<= monthDaysArr[yue]; s++){
+
+                if(s < p){
+                    dataLino.text(s);
+                    $('.data-date').append(dataLino.clone())
+                }else{
+                    dataLi3.text(s);
+                    $('.data-date').append(dataLi3.clone())
+                }
+
+            }
+        }
+    }
+
+
+
+
+}
+
+// 选择月
+function selectMonth(event){
+    if(isNaN(parseInt(sYear.n))){
+    }else{
+        oDate.setFullYear(parseInt(sYear.n));
+    }
+    sYear.y= event.innerHTML;
+    sYear.d=null;
+    oDate.setMonth(parseInt(sYear.y)-1);
+    // debugger
+    if(niankk < dataBegin){
+        if(sYear.n == niankk && parseInt(sYear.y) == month+1){
+            tian(date)
+        }
+        else if(sYear.n == dataBegin &&  parseInt(sYear.y) == month+1){
+            tian(date)
+        }
+        else{
+            tian()
+        }
+    }else{
+        if(sYear.n == niankk && parseInt(sYear.y) == month+1){
+            tian(date)
+        }
+        else if(sYear.n == dataBegin &&  parseInt(sYear.y) == month+1){
+            tian(date)
+        }
+        else{
+            tian()
+        }
+    }
+
+
+
+    $(event).addClass('current').siblings().removeClass('current');
+}
+// 选择天
+function selectDay(event) {
+    sYear.d= event.innerHTML;
+    // debugger
+    $(event).addClass('current').siblings().removeClass('current');
+}
+
+
+// 确定
+function idDate() {
+    if(sYear.n && sYear.y && sYear.d){
+        hideTime();
+        console.log(sYear)
+        return sYear;
+    }else{
+        titleMsg('日期选择不正确')
+        return false;
+
+    }
+
+}
+// 取消
+function hideTime() {
+    $('.data-select').slideUp()
+    $('#data-mask').hide()
+}
---
+++ js/public.js
@@ -0,0 +1,199 @@
+
+
+
+
+$.fn.extend({
+    // 图片上传
+    uploadImg:function(options){
+        let _self = this,
+            _this = $(this);
+        let defaults = {type:'checkbox',len:5,};
+        let settings = $.extend({},defaults, options);
+        _self.checkImg = function (img) {
+            var size = img / 1024
+            // 验证图片格式
+            if(!/image\/\w+/.test(img.type)){
+                alert("请确保文件为图像类型");
+                return false;
+            }
+            //验证图片大小
+            if(size>5000){
+                alert("单张图片不能大于5M");
+                return false;
+            }
+
+        };
+        _this.change(function () {
+            for(let i = 0;i<this.files.length;i++){
+                _self.checkImg(this.files[i]) //验证图片
+                console.log(settings.id)
+                let reader = new FileReader();
+                reader.readAsDataURL(this.files[i]);
+                reader.onload = function(e){
+                    console.log(settings.id)
+                    if(settings.type === 'checkbox'){
+                        let item = ' <div class="item">\n' +
+                            '<img src="'+this.result+'" alt="">\n' +
+                            '<div class="delete-img" onclick="deleteImg(this)"><s></s></div>'
+                        '</div>'
+                        if(_this.parent().siblings('.item').length + 1 > settings.len){
+                            _this.parent().hide()
+                        }else{
+                            _this.parent().before(item)
+                            if(_this.parent().siblings('.item').length === settings.len){
+                                _this.parent().hide()
+                            }
+                        }
+                    }
+                    else {
+                        let itemId = ' <img src="'+this.result+'" alt="">';
+                        _this.parent().after(itemId);
+                        _this.parent().siblings('.delete-load-id').show();
+                        _this.parent().siblings('.id-bj').hide();
+                        _this.parent().hide();
+                    }
+                }
+            }
+
+        })
+    },
+
+    // 等待付款倒計時
+    countDown: function (options) {
+        let _self = this,
+            _this = $(this);
+        let defaults = {second:3600,hour:false,num:0,isShowH:false};
+        let settings = $.extend({}, defaults, options);
+
+        _self.addZero = function (e) {
+            if(parseInt(e)  <= 9){
+                return  "0"+ parseInt(e)
+            }else{
+                return e
+            }
+        };
+        _self.timer = function () {
+            setInterval(function (){_self.second()}, 1000);
+        };
+        _self.second = function () {
+
+            let minH =  Math.floor(settings.second / 3600)
+            let minutes = Math.floor(settings.second / 60);
+            let seconds = Math.floor(settings.second % 60);
+            let msg;
+            if(settings.isShowH){
+
+
+                if(minH < 1){
+                    msg = _self.addZero(minutes) + ":" +  _self.addZero(seconds);
+                }else{
+                    msg = minH +'小时';
+                }
+                _this.text(msg);
+                --settings.second;
+            }
+            else if (settings.second >= 0) {
+                if(settings.hour){
+                   if(minH === 1){
+                       minutes = minutes - 60
+                   }
+                    msg = _self.addZero(minH) + ":" +_self.addZero(minutes) + ":" +  _self.addZero(seconds);
+                }
+                else{
+                    msg = _self.addZero(minutes) + ":" +  _self.addZero(seconds);
+                }
+                _this.text(msg);
+                --settings.second;
+            }
+            else{
+                settings.num++
+                if(settings.num === 1){
+                    clearInterval(_self.timer);
+                    titleMsg("时间到，结束!");
+                }
+
+            }
+        };
+        _self.timer()
+    },
+
+    //验证
+    verification:function (options) {
+        let _self = this,
+            _this = $(this);
+        let defaults = {num:0};
+        let settings = $.extend({}, defaults, options);
+        let isTrue = true
+        _self.isNull = function () {
+            _this.find('[pattern]').each(function (index,current) {
+                if(current.getAttribute('pattern').split(',')[0] == 1){
+                    if(current.value === "" && current.value.trim() === "" ){
+                        isTrue = false
+                        titleMsg(current.getAttribute('label')+'不能为空')
+                        return false;
+                    }
+                }
+            })
+        };
+        _self.isOk = function () {
+            _this.find('[pattern]').each(function (index,current) {
+                if (current.getAttribute('pattern').split(',')[1] && !eval(current.getAttribute('pattern').split(',')[1]).test(current.value)) {
+                    isTrue = false
+                    titleMsg(current.getAttribute('label') + '输入有误')
+                    return false;
+                }
+            })
+        };
+        _self.isNull();
+        if(isTrue){
+            _self.isOk()
+        }
+        if(isTrue){
+            return true
+        }else{
+            return false
+        }
+
+    },
+
+
+
+    //年月日时间选择
+    selectDate: function (options) {
+        let _self = this,
+            _this = $(this);
+        let defaults = {num:0};
+        let settings = $.extend({}, defaults, options);
+        data()
+    }
+
+});
+
+
+
+
+
+
+// 提示信息
+function titleMsg(msg) {
+    let titleText =$(" <!--报错信息-->\n" +
+        "  <div class=\"title-msg\" onclick=\"this.style.display='none'\">\n" +
+        "    <div class=\"col\">\n" + msg+
+        "    </div>\n" +
+        "  </div>")
+    let titleDom= $('.title-msg');
+    if($(document.body).find(titleDom) && $(document.body).find(titleDom).length === 0){
+        $(document.body).append(titleText)
+    }
+    $('.title-msg .col').text(msg)
+    $('.title-msg').show();
+    setTimeout(function () {
+        $('.title-msg').hide()
+    },3000)
+}
+
+
+
+
+
+
